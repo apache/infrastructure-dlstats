163270d1dc0a075ba0503423419e6bc4ac405ceb
Daniel Gruno <humbedooh@apache.org>
2021-10-26 22:26:13 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import ahapi
+import asyncio
+
+httpserver = ahapi.simple(
+    api_dir="/opt/dlstats/scripts",    # serve api end points from here
+    static_dir="/opt/dlstats/htdocs",  # serve stuff like html and images from here
+    bind_ip="127.0.0.1",
+    bind_port=8080,
+    state={"something": "stateful"}
+)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(httpserver.loop())
