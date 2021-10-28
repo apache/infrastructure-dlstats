0feb31f60e7dad334aa91d07ef97c85f2f6b1d9f
Daniel Gruno <humbedooh@apache.org>
2021-10-29 01:34:21 +0200

    needs to be match

--- scripts/stats.py
+++ scripts/stats.py
@@ -44,7 +44,7 @@ async def process(state: typing.Any, request, formdata: dict) -> dict:
     for provider, field_names in FIELD_NAMES.items():
         q = elasticsearch_dsl.Search(using=es_client)
         q = q.filter("range", **{field_names['timestamp']: {"gte": f"now-{duration}"}})
-        q = q.filter("term", **{field_names['request_method']: "GET"})
+        q = q.filter("match", **{field_names['request_method']: "GET"})
         q = q.filter("range", bytes={"gt": 5000})
         q = q.filter("match", **{field_names['uri']: project})
         q = q.filter("regexp", **{field_names['uri'] + ".keyword": r".*\.[a-z0-9]+"})
